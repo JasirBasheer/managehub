b588f8f88736b137fe9d793791b6a2f88bcb80a9
JasirBasheer <jasirbinbasheerpp@gmail.com>
2024-08-20 21:27:34 +0530

    add Member

--- model/coordinatorSchema.js
+++ model/coordinatorSchema.js
@@ -1,1 +1,26 @@
-console.log('coordinator schema');
+const mongoose = require('mongoose')
+
+const coordinatorSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    batch:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String
+    }
+    
+})
+
+module.exports= mongoose.model('Coordinator',coordinatorSchema)
